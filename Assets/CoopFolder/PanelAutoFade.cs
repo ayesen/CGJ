63056b871594f3fac9470a9170ee8e71f69c81e6
Cooper Yang <ty1037@nyu.edu>
2021-04-05 02:20:59 -0400

    fade

---
+++ Assets/CoopFolder/PanelAutoFade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelAutoFade : MonoBehaviour
+{
+    private bool mFaded = false;
+    public float duration = .4f;
+
+    public void Fade()
+    {
+        var canvGroup = GetComponent<CanvasGroup>();
+        StartCoroutine(DoFade(canvGroup, canvGroup.alpha, mFaded ? 1 : 0));
+        mFaded = !mFaded;
+        GameObject.Find("UDLRManager").GetComponent<UDLRManager>().ChangeQuestions();
+        StartCoroutine(DoFade(canvGroup, canvGroup.alpha, mFaded ? 1 : 0));
+        mFaded = !mFaded;
+        
+    }
+
+    public IEnumerator DoFade(CanvasGroup canvGroup, float start, float end)
+    {
+        float counter = 0f;
+        
+        while (counter<duration)
+        {
+            counter += Time.deltaTime;
+            canvGroup.alpha = Mathf.Lerp(start, end, counter / duration);
+            
+            yield return null;
+        }
+    }
+}
---
+++ Assets/CoopFolder/SceneFadeOut.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneFadeOut : MonoBehaviour
+{
+    private bool mFaded = true;
+    public float duration = .4f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fade();
+        }
+    }
+
+    public void Fade()
+    {
+        var canvGroup = GetComponent<CanvasGroup>();
+        StartCoroutine(DoFade(canvGroup, canvGroup.alpha, mFaded ? 1 : 0));
+        mFaded = !mFaded;
+    }
+
+    public IEnumerator DoFade(CanvasGroup canvGroup, float start, float end)
+    {
+        float counter = 0f;
+        
+        while (counter<duration)
+        {
+            counter += Time.deltaTime;
+            canvGroup.alpha = Mathf.Lerp(start, end, counter / duration);
+            
+            yield return null;
+        }
+    }
+    
+}
