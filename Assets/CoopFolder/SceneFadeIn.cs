d8021e72e1448bfe02e72a3b79634a63df328892
Cooper Yang <ty1037@nyu.edu>
2021-04-05 03:25:36 -0400

    no message

---
+++ Assets/CoopFolder/SceneFadeIn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneFadeIn : MonoBehaviour
+{
+    private bool mFaded = false;
+    public float duration = .4f;
+
+    private void Start()
+    {
+        Fade();
+    }
+
+    public void Fade()
+    {
+        var canvGroup = GetComponent<CanvasGroup>();
+        StartCoroutine(DoFade(canvGroup, canvGroup.alpha, mFaded ? 1 : 0));
+        mFaded = !mFaded;
+    }
+
+    public IEnumerator DoFade(CanvasGroup canvGroup, float start, float end)
+    {
+        float counter = 0f;
+        
+        while (counter<duration)
+        {
+            counter += Time.deltaTime;
+            canvGroup.alpha = Mathf.Lerp(start, end, counter / duration);
+            
+            yield return null;
+        }
+    }
+}
